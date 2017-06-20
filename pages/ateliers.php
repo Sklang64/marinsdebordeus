baee39f3f106f581df986039a83056a7f8d17467
Gauthier-Lafaye <vgauthier.lafaye@gmail.com>
2017-06-20 15:46:07 +0200

    Ajout des fichiers CSS ateliers et master ainsi que de certains éléments graphique

---
+++ pages/ateliers.php
@@ -0,0 +1,60 @@
+<?php include("header.php"); ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Ateliers</title>
+    <link rel="stylesheet" href="../assets/css/master.css">
+    <link rel="stylesheet" href="../assets/css/ateliers.css">
+  </head>
+  <body>
+
+
+    <section id="bandeau">
+      <img src="../assets/img/creabandeau.jpg" alt="Rénovation du Créa">
+      <h2>Lorem Ipsum</h2>
+      <p>
+        Primi igitur omnium statuuntur Epigonus et Eusebius ob nominum gentilitatem oppressi.<br/>
+        praediximus enim Montium sub ipso vivendi termino his vocabulis appellatos fabricarum.<br/>
+        culpasse tribunos ut adminicula futurae molitioni pollicitos.
+      </p>
+    </section>
+
+    <section id="crea">
+        <div class="slider">
+          <img src="../assets/img/crea_mise_a_eau.jpg" alt="Mise à l'eau du Créa">
+
+        </div>
+
+        <div class="contenu">
+          <h2>Le Créa</h2>
+          <p>Cotre bermudien de 12m en bois.<br>
+   C’est un plan Sergent construit en 1963 par le Chantier Rameau à Etel et baptisé Bengali. C’était un racer destiné aux courses-croisières, en particulier celles du RORC. Il a brillé dans de nombreuses régates: Falmouth-Douarnenez, Cowes, Fasnet…Le bateau a ensuite appartenu, sous le nom Thétys 3, à une médecin de marine qui servait sur les ferries transmanche. Il est alors revenu à Bordeaux oû il a peu navigué et s’est petit à petit dégradé.
+    Daniel Lys, adhérent de Hissez Haut, l’a racheté en 1992 et a entrepris de le restaurer. Le pont, le rouf, le lest et les aménagements intérieurs ont été refaits. Rebaptisé Créa (nom local de l’esturgeon), le bateau a beaucoup navigué (rassemblements de Brest, Coupe des deux phares…).  sauvé d’un incendie alors qu’il était en maintenance il a été racheté par Mathieu Lemunier et ses frères et sa coque a entièrement été refaite par le charpentier de marine Yvan de Parada dans l’atelier des Marins de Bordeùs. Actuellement basé au Musée Maritime de La Rochelle, le Créa participe à de nombreues navigations.
+   La vie du bateau depuis le rachat par les frères Lemunier est détaillée dans le blog crea sergent.blogspot.com.s
+          </p>
+        </div>
+    </section>
+
+    <section id="mehala">
+        <div class="slider">
+
+        </div>
+
+        <div class="contenu">
+
+        </div>
+    </section>
+
+    <section id="gitana">
+        <div class="slider">
+
+        </div>
+
+        <div class="contenu">
+
+        </div>
+    </section>
+
+  </body>
+</html>

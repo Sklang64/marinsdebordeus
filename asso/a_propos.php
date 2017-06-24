e4ffd7cdc9326a1213d93a277d0258ef74939e39
jesspra <jessie.pradeau@gmail.com>
2017-06-24 16:39:53 +0200

    Add files via upload

---
+++ asso/a_propos.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>A propos</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="assets/css/style.css" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans|Open+Sans+Condensed:300" rel="stylesheet">
+
+</head>
+<body>
+
+  <?php
+  include("header.php");
+  ?>
+
+  <!--Historique-->
+  <!--Photo premier plan -->
+  <section id="history">
+    <img class="picture" src="assets/img/bg_about01.jpg" alt="photo_bateau" title="photo Les Marins de Bordeus"/>
+
+    <!--Histoire assos -->
+      <h1>HISTOIRE DE MARINS</h1>
+        <article class="assos">
+      <!--Hissez haut-->
+          <div class="asso">
+            <h2>Le début : <br> Hissez Haut Marins de Gascogne</h2>
+            <p>L'association est née en 1989 à Meschers (port à côté de Royan).
+              Créés par René Lys, Bernard Lacampagne, officier de marine marchande, Yvon Gréaud, patron lamaneur et Georges Portal.
+              En 1990, l'association s'installe à Bordeaux au Hangar 26 du Bassin à flot n°2 et commence vraiment l'activité de restauration de bateaux. Une trentaine de bateaux a été restaurée en vingt ans. Le Teazer, voilier anglais de 1880, le Gitana III, ancien yacht des Rotschild, le Créa, plan Sergent des années soixante avec un beau palmarès ainsi que de nombreux bateaux de pêche.</p>
+            <img src="assets/img/hissez_haut.jpg" alt="photo_hissez_haut">
+          </div>
+
+      <!--Les Marins de Bordeus-->
+          <div class="asso">
+            <h2>La relève : <br> Les Marins de Bordeus </h2>
+            <p>L'association Les Marins de Bordeus sont les héritiers d'Hissez haut. L'activité est toujours la restauration du patrimoine maritime et fluviale.
+              Elle est actuellement logée au Hangar 29. L'association participe aux manifestations maritimes comme La semaine du Golfe du Morbihan, Les Voiles d'estuaire à Royan, La RBBAIV des Chantiers de Tramasset (La Rencontre des Bateaux en Bois et Autres Instruments à Vent...).
+              D'autre part, l'association milite pour le maintien des activités nautiques des Bassins à Flots et de Bacalan en général en collaboration avec les autres associations nautiques du site.
+              Elle est de ce fait une interlocutrice auprès des Institutions locales.</p>
+            <img src="assets/img/marins.jpg" alt="/photo_marins_bordeus">
+         </div>
+       </article>
+
+   </section>
+
+   <section class="team">
+     <h2>Notre équipage</h2>
+      <div class="imgs">
+        <img src="assets/img/michel1.jpg" alt="photo_marins">
+        <img src="assets/img/jean-marie.jpg" alt="photo_marins">
+        <img src="assets/img/lea.jpg" alt="photo_marins">
+        <img src="assets/img/michel2.jpg" alt="photo_marins">
+     </div>
+        <p>"Les Marins de Bordeus, attentifs et vigilants, donnent de leur énergie et de leur amour pour les anciens bateaux délaissés de tous, pauvres animaux épuisés et blessés, voués à une mort silencieuse.</p>
+        <p>Ils tournent autour du voilier malade qu’ils ont installé sur de solides béquilles. Ils observent minutieusement les balafres et les cicatrices du temps, commentent le travail des anciens, réfléchissent aux réparations les mieux adaptées.</p>
+        <p>Ils grattent les couleurs écaillées, mettent le bois à nu, colmatent les brèches, polissent les flancs bombés, réparent les parties rongées, posent des attelles.</p>
+        <p>Massive et sombre, la base sous-marine, témoin d’un temps révolu, observe jour après jour les Marins de Bordeus, attentifs et affairés.</p>
+        <p>Tapie dans les ténèbres de la nuit, émergée des fonds aquatiques, ses lourdes pattes nimbées de lumineux faisceaux rouges, elle veille sur ces fragiles bateaux en convalescence, oiseaux déplumés, immobilisés sur leurs longues échasses, au bord du Bassin à flots.</p>
+        <p>Les Marins de Bordeus appliquent de nouvelles peintures soulignant la courbe des lignes sur lesquelles s’accrochent maintenant les lumières du ciel et les reflets de l’eau.</p>
+        <p>Spécialistes et badauds admirent l’étonnante résurrection de l’embarcation, sa nouvelle beauté d’autant plus resplendissante qu’elle a été arrachée à la décadence, arrachée à la disparition.</p>
+        <p>Les Marins de Bordeus, guérisseurs de vieilles carcasses marines.</p>
+        <p>Les Marins de Bordeus, pourvoyeurs de vie, pourvoyeurs de rêves."</p>
+        <p><strong>Louise Gabriel - 2017</strong></p>
+   </section>
+
+   <!--Parallaxe-->
+   <section class="parallax">
+     <div class="parallax p1" title="Augustin Terpereau-Les Bassins à Flots">
+       <h2>Les Bassins à flot<br>
+         200 ans d'histoire</h2>
+     </div>
+   </section>
+
+  <!--Les Bassins à Flots -->
+  <section class="archive">
+    <div class="text">
+      <h2>Héritage maritime bordelais</h2>
+      <p>Les Bassins à flot ont profondément marqué l'histoire de Bordeaux et son quartier de Bacalan. Fort d'un passé maritime, industriel et marchand, le nouveau quartier répond à une nouvelle dynamique économique orientée vers le fleuve.
+         Au fil du temps, le quartier a su se renouveler tout en gardant son authenticité liée à la mer.
+         De la construction du premier bassin au XIXème siècle à l'élaboration du nouveau quartier résidentiel, en passant par la construction de la Base sous Marine pendant l'Occupation allemande, les Bassins à flot ont su préservé leur identité forte marquée par leur héritage maritime.</p><br>
+    </div>
+     <figure class="photo"><img src="assets/img/archive_Bassin_a_flot.jpg" alt="photo_bassin_a_flot" title="Photo Archives Grand Port Maritime de Bordeaux"></figure>
+  </section>
+
+
+  <!--Scroll-->
+    <a id="scroll-top" href="#top"><button><img src="assets/img/arrow-next-vertical-icon-64.png" alt="scroll" width="20px"></button></a>
+      <script src='https://code.jquery.com/jquery-2.2.4.min.js'></script>
+      <script src="assets/js/a_propos.js"></script>
+
+
+</body>
+</html>
---
+++ asso/header.php
@@ -0,0 +1,31 @@
+<header>
+
+
+    <a href="index.php"><img src="assets/img/logo-marins-bordeus.png" alt="logo_marins_de_bordeus"/></a>
+    <figure id="bridges">
+    <img src="assets/img/illustr-bridges.png" style="width: 300px;"/>
+    </figure>
+
+    <!-- Search Bar-->
+    <div class="wrap">
+      <div class="search" role="search">
+        <input type="search" class="searchTerm" placeholder="Rechercher">
+        <button type="submit" class="searchButton">OK</button>
+      </div>
+
+    </div>
+
+  </header>
+
+
+  <!-- Navigation-->
+
+  <nav id="nav">
+    <ul>
+      <li><a href="#">Accueil</a></li>
+      <li><a href="#section1">A propos</a></li>
+      <li><a href="#">Ateliers</a></li>
+      <li><a href="#">Adhérer Soutenir</a></li>
+      <li><a href="#section7">contact</a></li>
+    </ul>
+  </nav>
